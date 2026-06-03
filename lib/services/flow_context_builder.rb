f4db70b037a1e50f035b5e44868138c84bedaa52
Setoju <setoju48@gmail.com>
2026-06-03 15:13:50 +0300

    feat: integrate scope inference into flow context builder

--- lib/services/flow_context_builder.rb
+++ lib/services/flow_context_builder.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'scope_inferrer'
+
 module Commiti
   module FlowContextBuilder
-    def self.build(flow_type:, diff:, client:, run_stage:, model:, text_generation_config: nil, worker_count: nil)
+    def self.build(flow_type:, diff:, client:, run_stage:, model:, text_generation_config: nil, worker_count: nil,
+                   style_profile: nil, inferred_scope: nil)
       line_chunks = Commiti::DiffParser.split_by_file_lines(diff)
       diff_metadata = Commiti::DiffParser.metadata_from_line_chunks(line_chunks)
       change_groups = Commiti::ChangeGrouping.group(line_chunks)
+      inferred_scope ||= infer_scope(flow_type, diff_metadata, style_profile)
 
       summarized_result = run_stage.call('Preparing diff for AI model') do
         Commiti::DiffSummarizer.summarize_if_needed(
@@ -23,7 +27,9 @@ module Commiti
         summarized: summarized_result[:summarized],
         raw_diff: diff,
         diff_metadata: diff_metadata,
-        style_config: text_generation_config
+        style_config: text_generation_config,
+        style_profile: style_profile,
+        inferred_scope: inferred_scope
       )
 
       {
@@ -38,5 +44,16 @@ module Commiti
       line_chunks.map { |chunk| { path: chunk[:path], diff: chunk[:lines].join } }
     end
     private_class_method :summary_chunks
+
+    def self.infer_scope(flow_type, diff_metadata, style_profile)
+      return nil unless flow_type == :commit
+      return nil unless style_profile
+
+      Commiti::ScopeInferrer.infer(
+        files: Array(diff_metadata&.dig(:files)),
+        common_scopes: style_profile.common_scopes
+      )
+    end
+    private_class_method :infer_scope
   end
 end
